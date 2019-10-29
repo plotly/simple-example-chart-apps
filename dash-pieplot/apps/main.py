61f9e23f50269e3dd2386b8f08e9ad52dd3e1f97
Thanos-Ant <57007814+Thanos-Ant@users.noreply.github.com>
2019-10-29 21:50:37 +0200

    Changes to the App
    I have changed some aspects of the app through the 'main.py' script, namely:
    - Converted the  to a drop-down menu
    - Added an if statement when trying to plot non-existing data (Displays a black pie chart, has a title " No Data to Display", "Country not Found" in the labels and as data 404)

--- dash-pieplot/apps/main.py
+++ dash-pieplot/apps/main.py
@@ -7,6 +7,7 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 from app import app
+from datetime import datetime as dt
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_ebola.csv')
 df = df.dropna(axis=0)
@@ -16,15 +17,23 @@ if 'DYNO' in os.environ:
 else:
     app_name = 'dash-pieplot'
 
+
+#mine
 layout = html.Div([
     html.Div([html.H1("Ebola Cases Reported in Africa - 2014")], style={"textAlign": "center"}),
-    dcc.Graph(id="my-graph"),
-    html.Div([dcc.Slider(id='month-selected', min=3, max=12, value=8,
-                         marks={3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September",
-                                10: "October", 11: "November", 12: "December"})],
-             style={'textAlign': "center", "margin": "30px", "padding": "10px", "width": "65%", "margin-left": "auto",
-                    "margin-right": "auto"}),
+    dcc.Dropdown(id='month-selected',
+        options=[
+            {'label': "March", 'value': 3}, {'label': "April", 'value': 4},{'label': "May", 'value': 5},
+            {'label': "June", 'value': 6},{'label': "July", 'value': 7},{'label': "August", 'value': 8},
+            {'label': "September", 'value': 9},{'label': "October", 'value': 10},{'label': "November", 'value': 11},
+            {'label': "December", 'value': 12}
+        ],
+        value=3,
+    ),
+
+    dcc.Graph(id="my-graph"),  
 ], className="container")
+
 
 
 @app.callback(
@@ -32,8 +41,18 @@ layout = html.Div([
     [dash.dependencies.Input("month-selected", "value")]
 )
 def update_graph(selected):
-    return {
+    
+    if df[df["Month"] == selected]["Value"].empty:
+        return {
+        "data": [go.Pie(labels=['Country Not Found'], values=[404],
+                        marker={'colors': ['#000000']}, textinfo='none')],
+        "layout": go.Layout(title="No Data to Display", margin={"l": 300, "r": 300, },
+                            legend={"x": 1, "y": 0.7})}
+    
+    else:
+        return {
         "data": [go.Pie(labels=df["Country"].unique().tolist(), values=df[df["Month"] == selected]["Value"].tolist(),
                         marker={'colors': ['#EF963B', '#C93277', '#349600', '#EF533B', '#57D4F1']}, textinfo='label')],
-        "layout": go.Layout(title=f"Cases Reported Monthly", margin={"l": 300, "r": 300, },
+        "layout": go.Layout(title="Cases Reported Monthly", margin={"l": 300, "r": 300, },
                             legend={"x": 1, "y": 0.7})}
+        
